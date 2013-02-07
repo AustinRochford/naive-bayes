0d0bd6d57318ee647ac58ee930ba0c0dc9bff475
Austin Rochford <austin.rochford@gmail.com>
2013-02-07 16:23:27 -0600

    Make generator_args with a list comprehension

--- make_data.py
+++ make_data.py
@@ -50,10 +50,8 @@ if __name__ == "__main__":
     args = make_argument_parser().parse_args()
     generator = locals()[args.generator + '_generator']
     generator_arg_types = locals()[args.generator + '_generator_arg_types']()
-    generator_args = []
+    generator_args = [type_(args) for type_, arg in zip(generator_arg_types, args.generator_args.split())]
 
-    for type_, arg in zip(generator_arg_types,args.generator_args.split()):
-        generator_args.append(type_(arg))
 
     for i in range(args.num_data):
         category, features = generator(*generator_args)
